5c09894842c1b65163c7baaaac6f1c312a147dde
lingjiancode <mXf30fHJLqEEXGbzdqLYvD+lingjiancode@noreply.cnb.cool>
2026-03-28 18:00:27 +0800

    fix: Dockerfile

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # build
-docker build --no-cache -t ${CNB_DOCKER_REGISTRY}/${CNB_REPO_SLUG_LOWERCASE}-cool .
+docker build --no-cache -t ${CNB_DOCKER_REGISTRY}/${CNB_REPO_SLUG_LOWERCASE}/cool .
 # push
-docker push ${CNB_DOCKER_REGISTRY}/${CNB_REPO_SLUG_LOWERCASE}-cool
+docker push ${CNB_DOCKER_REGISTRY}/${CNB_REPO_SLUG_LOWERCASE}/cool
