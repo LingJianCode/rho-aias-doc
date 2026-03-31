4a84191b10ea05fa4799468bd9e8063bbf39084c
lingjiancode <mXf30fHJLqEEXGbzdqLYvD+lingjiancode@noreply.cnb.cool>
2026-03-31 20:38:39 +0800

    vitepress

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -89,7 +89,7 @@ export default withMermaid({
     ],
 
     footer: {
-      message: '基于 eBPF/XDP 技术构建',
+      message: '基于 vitepress 技术构建',
       copyright: `Copyright © 2026-${new Date().getFullYear()} 备案号：<a href="https://beian.miit.gov.cn/" target="_blank">蜀ICP备2026014678号-1</a>`
     },
 
